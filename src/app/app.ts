4e6e85c2e22e7d99f2cc492bd8b829f33c920289
Erik Garces Matos <erik_senac@hotmail.com>
2025-06-21 11:29:13 -0300

    feat: Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./_components/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'gerador-certificado';
+  exibeNavbar: boolean = true;
 }

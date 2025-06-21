3cb275456f7fede9765f3f5c44d7006d98650dd0
Erik Garces Matos <erik_senac@hotmail.com>
2025-06-21 11:46:55 -0300

    feat: Adding component buttom

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Navbar } from "./_components/navbar/navbar";
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule],
+  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

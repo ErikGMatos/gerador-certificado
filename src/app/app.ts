5b1e89e3edbf645354c53f0f7341a56c3c47eb95
Erik Garces Matos <erik_senac@hotmail.com>
2025-06-22 21:10:52 -0300

    feat: Adding certificate component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BaseUi } from "./_components/base-ui/base-ui";
 import { Navbar } from "./_components/navbar/navbar";
+import { Certificado } from "./pages/certificado/certificado";
 import { CertificadoForm } from "./pages/certificado-form/certificado-form";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, BaseUi, CertificadoForm],
+  imports: [Navbar, CommonModule, BaseUi, Certificado, CertificadoForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

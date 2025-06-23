c1575add79c46279a4d59c3de64e654a246075c9
Erik Garces Matos <erik_senac@hotmail.com>
2025-06-23 14:06:40 -0300

    feat: Adding certfied interface

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -3,6 +3,7 @@ import { SecondaryButton } from "../../_components/secondary-button/secondary-bu
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificado-form',
@@ -11,24 +12,33 @@ import { CommonModule } from '@angular/common';
   styleUrl: './certificado-form.css'
 })
 export class CertificadoForm {
-  nome = ''
   atividade = ''
-  atividades: string[] = []
+
+  certificado: Certificado = {
+    atividades: [],
+    nome: '',
+  };
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched
   }
 
   formValido() {
-    return this.nome.length > 0 && this.atividades.length > 0;
+    return this.certificado.nome.length > 0 && this.certificado.atividades.length > 0;
   }
 
   adicionarAtividade() {
-    this.atividades.push(this.atividade)
+    this.certificado.atividades.push(this.atividade)
     this.atividade = ''
   }
 
   excluirAtividade(index: number) {
-    this.atividades.splice(index, 1)
+    this.certificado.atividades.splice(index, 1)
+  }
+
+  submit() {
+    if(!this.formValido){
+      return
+    }
   }
 }
